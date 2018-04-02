22fa6b06094e43da4081d975e3456c51f6f565a4
Jurica Cerovec <jurica.cerovec@microblink.com>
2018-04-02 21:39:50 +0200

    Added dummy ios app project

---
+++ not-hotdog/MBRootViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  not-hotdog
+//
+//  Created by Jura on 02/04/2018.
+//  Copyright © 2018 MicroBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBRootViewController : UIViewController
+
+
+@end
+
---
+++ not-hotdog/MBRootViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  not-hotdog
+//
+//  Created by Jura on 02/04/2018.
+//  Copyright © 2018 MicroBlink. All rights reserved.
+//
+
+#import "MBRootViewController.h"
+
+@interface MBRootViewController ()
+
+@end
+
+@implementation MBRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
